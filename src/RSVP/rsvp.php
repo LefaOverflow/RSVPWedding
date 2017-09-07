12c36dde6fba9e4bcd0452e20c2ba0e2e4858852
Lefa Mashele <Lefa Mashele>
2017-09-07 17:05:56 +0200

    Initial Files

---
+++ src/RSVP/rsvp.php
@@ -0,0 +1,11 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+$app = new \Slim\App;
+
+//Add Guest
+$app->get('/api/RSVP/AddGuest', function (Request $request, Response $response) {
+
+
+});
